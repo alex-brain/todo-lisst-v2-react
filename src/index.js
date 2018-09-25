1271758e0c09b1d74606728e0ff6d63e1bcb7f6e
alex-brain <cm-brain@yandex.ru>
2018-09-26 00:24:49 +0300

    Add redux-persist

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import store from './store/store.js';
+import configureStore from './store/store.js';
 import App from './containers/app/index';
+import { PersistGate } from 'redux-persist/integration/react';
 import registerServiceWorker from './registerServiceWorker';
 import './theme/style.scss';
 
+const { store, persistor } = configureStore();
+
 ReactDOM.render(
   <Provider store={store}>
-    <App/>
+    <PersistGate loading={null} persistor={persistor}>
+      <App/>
+    </PersistGate>
   </Provider>,
   document.getElementById('root'));
 registerServiceWorker();
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,22 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
-import * as reducers from './reducers';
 import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import * as reducers from './reducers';
 
-export default createStore(
-  combineReducers(reducers),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(thunk)
-);
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, combineReducers(reducers));
+
+export default () => {
+  let store = createStore(
+    persistedReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk)
+  );
+  let persistor = persistStore(store);
+  return { store, persistor };
+}
